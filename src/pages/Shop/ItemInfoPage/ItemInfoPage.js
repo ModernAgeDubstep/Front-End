518bff55fa700c0647fa74fe924a98933f50a799
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2021-05-13 21:53:25 -0400

    clear warnings, fix deployment

--- src/pages/Shop/ItemInfoPage/ItemInfoPage.js
+++ src/pages/Shop/ItemInfoPage/ItemInfoPage.js
@@ -1,17 +1,19 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchProductAndPrice } from '../../../state/actions/index';
 
 const ItemInfoPage = () => {
-    const productInfo = useSelector(state => state.productInfo);
+    // const productInfo = useSelector(state => state.productInfo);
     const dispatch = useDispatch();
     const Params = useParams();
 
     useEffect(() => {
         fetchProductAndPrice(dispatch, Params.productId)
-    }, [])
-    
+    }, [Params.productId])
+    // Create the style for this page:
+    // complete page features.
+    // switch API endpoint to live version.
     return(
         <>
             <h1>Item Info Page</h1>
